6d2ec04b8e342572f0fcfa93f53dcf0dcbd05ddc
MikolajUz <muzialko@gmail.com>
2023-11-23 12:39:30 +0100

    battlefield grid covers whole container, no color, no grid lines

--- src/app/main/components/features/battlefield/battlefield.component.ts
+++ src/app/main/components/features/battlefield/battlefield.component.ts
@@ -1,16 +1,6 @@
-import {
-  Component,
-  ElementRef,
-  OnInit,
-  ViewChild,
-  OnChanges,
-  SimpleChanges,
-  Input,
-  HostListener,
-} from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { UnitDirective } from 'src/app/players/components/features/unit.directive';
 import { FacadeService } from 'src/app/facade/facade.service';
-import { concatAll } from 'rxjs';
 import { ResizedEvent } from 'angular-resize-event';
 
 @Component({
@@ -24,9 +14,10 @@ export class BattlefieldComponent {
 
   width = 0;
   height = 0;
-
   gridUnit: number = 10;
-  grids = this.facade.createGridArray(50);
+  gridDensity = 60;
+  gridsX = this.facade.createGridArray(this.gridDensity);
+  gridsY = this.facade.createGridArray(this.gridDensity);
   gridUnitScss = `${this.gridUnit}px`;
 
   constructor(private facade: FacadeService) {}
@@ -42,12 +33,13 @@ export class BattlefieldComponent {
   onResized(event: ResizedEvent): void {
     this.width = Math.round(event.newRect.width);
     this.height = Math.round(event.newRect.height);
-    this.gridUnit = this.width / 50;
-    let gridNumber: number;
-    this.width >= this.height
-      ? (gridNumber = Math.round(this.height / this.gridUnit))
-      : (gridNumber = Math.round(this.width / this.gridUnit));
-    this.grids = this.facade.createGridArray(gridNumber);
+    this.gridUnit = this.width / this.gridDensity;
+    this.gridsX = this.facade.createGridArray(
+      Math.round(this.width / this.gridUnit)
+    );
+    this.gridsY = this.facade.createGridArray(
+      Math.round(this.height / this.gridUnit)
+    );
     this.gridUnitScss = `${this.gridUnit}px`;
     this.facade.setGridUnit(
       this.gridUnit,
